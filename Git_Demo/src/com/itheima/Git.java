b4b9dccfd5b6452933675fb670f4433c2ac2695d
root <1843138901@qq.com>
2019-02-23 21:00:29 +0800

    添加test工程至Git

--- Git_Demo/src/com/itheima/Git.java
+++ Git_Demo/src/com/itheima/Git.java
@@ -2,11 +2,6 @@ package com.itheima;
 
 public class Git {
     public static void main(String [] args){
-        System.out.println("hello Git");
-        System.out.println("hello Git");
-        System.out.println("hello Git");
-        System.out.println("hello Git");
-        System.out.println("hello Git");
-        System.out.println("hello Git");
+
     }
 }
--- Git_Demo02/src/com/itheima/Demo.java
+++ Git_Demo02/src/com/itheima/Demo.java
@@ -2,6 +2,6 @@ package com.itheima;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("这又是一个新的项目");
+        System.out.println("这又是一个新的好的项目");
     }
 }
